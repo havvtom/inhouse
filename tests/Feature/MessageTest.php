a14f285a065ceb02003b0210e90048dd776f80f1
havvtom <45094979+havvtom@users.noreply.github.com>
2023-02-27 08:54:19 +0200

    Update MessageTest.php

--- tests/Feature/MessageTest.php
+++ tests/Feature/MessageTest.php
@@ -111,33 +111,5 @@ class MessageTest extends TestCase
             'created_by_id' => $john->id
         ]);
     }
-
-    // public function test_admin_users_can_reply_to_a_message(){
-    //     //create a user
-    //     $user = User::factory()->create(['name' => 'John']);
-    //     //give them admin role
-    //     $role = Role::factory()->create();
-
-    //     $user->roles()->save($role);
-
-    //     //sign them in 
-    //     $this->be($user);
-
-    //     //create a message
-    //     $message = Message::factory()->create(['created_by_id' => $user->id]);
-
-    //     $this->assertDatabaseHas('messages', [
-    //         'message' => $message->message
-    //     ]);
-
-    //     //check if the admin user can reply to the message
-        
-    //     $response = $this->getJson(route('messages.show', 2));
-    //     // $response = $this->delete('messages/'.$message->id);
-    //     $response->assertOk();
-    //     // dd($response->content());
-
-    //     // $response->assertSee('John');
-    // }
     
 }
